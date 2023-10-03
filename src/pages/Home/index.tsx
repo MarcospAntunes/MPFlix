0031581bb5a2f8adf3b3d718862fe47c113b9333
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2023-10-03 19:47:00 -0300

    Criação do esqueleto da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,21 @@
+import { ContainerDiv } from "../../components/Containers/ContainerDiv"
+import Header from "../../components/Header"
 
 
 function Home() {
   return (
     <>
-    
+      <ContainerDiv>
+        <Header />
+        <main>
+          <section></section> {/* Seção do banner */}
+          <section></section> {/* Seção dos filmes */}
+        </main>
+        <footer>
+          <h2>Desenvolvidor por Marcos Antunes</h2>
+          <h2>Design feito por <a href="https://dribbble.com/apurple" target="_blank">aPurple</a></h2>
+        </footer>
+      </ContainerDiv>
     </>
   )
 }
