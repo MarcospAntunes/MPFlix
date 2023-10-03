dbf6d5f6bc15e9d31c7622dbfd0d8b3d47f500bd
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2023-10-03 19:45:32 -0300

    Criação do Container Div
    Este container será responsável por organizar o conteúdo das páginas.

---
+++ src/components/Containers/ContainerDiv.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ContainerDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100vw;
+    height: 100vh;
+
+`
+
+export { ContainerDiv }
