aa4398feb3b40609f136fb06fae1b689661e091f
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2023-10-04 20:10:50 -0300

    Criado o componente Caixa de Pesquisa

---
+++ src/components/CaixaDePesquisa/index.tsx
@@ -0,0 +1,54 @@
+import { BiSearch } from 'react-icons/bi'
+import styled from 'styled-components'
+
+const DivBusca = styled.div`
+    width: 300px;
+    height: 40px;
+    background-color: #363636;
+    border-radius: 50px;
+    margin: 10px 10px 10px 0px;
+
+    input {
+        float: right;
+        background-color: transparent;
+        padding-right: 5px;
+        font-style: italic;
+        border: none;
+        height: 100%;
+        width: 260px;
+        border-radius: 50px;
+        color: white;
+
+        &:focus {
+            box-shadow: 0 0 0 0;
+            border: 0 none;
+            outline: 0;
+        }
+    }
+
+    svg {
+        width: 20px;
+        height: 20px;
+        margin: 10px;
+        cursor: pointer;
+    }
+`
+
+function chamarInput() {
+    const chamarInput: HTMLInputElement = document.querySelector('#textBusca')!
+    chamarInput.focus()
+}
+
+function CaixaDePesquisa() {
+    return(
+        <DivBusca>
+            <BiSearch className="btnBusca" onClick={() => chamarInput()}/>
+          <input type="text" id="textBusca" placeholder="Search here" />
+
+        </DivBusca>
+    )
+
+}
+
+
+export default CaixaDePesquisa
