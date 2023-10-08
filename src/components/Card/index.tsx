750916f78fd90d59b666c3911c06937cce0aa2a6
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2023-10-08 13:57:33 -0300

    Adicionado Modal para o Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,7 @@
 import styled from "styled-components"
+import ModalMovie from "./ModalMovie"
+import { useState } from "react"
 
-interface CardProps {
-    id: number
-    genre_ids: number
-    poster: string
-    title: string
-    release_date: string
-}
 
 const ImgStyled = styled.img`
     border-radius: 10px;
@@ -21,12 +16,35 @@ const ImgStyled = styled.img`
     }
 `
 
+interface CardProps {
+    id: number
+    genre_ids: number
+    poster: string
+    title: string
+    release_date: string
+    overview: string
+    vote_average: number
+}
 
-function Card({id, genre_ids, poster, title, release_date}: CardProps) {
+
+
+function Card({id, genre_ids, poster, title, release_date, overview, vote_average}: CardProps) {
+    const [openModal, setOpenModal] = useState(false)
+
     return(
         <li key="id">
-            <ImgStyled src={`https://image.tmdb.org/t/p/w300/${poster}`}/>
-            
+            <ImgStyled src={`https://image.tmdb.org/t/p/w300/${poster}`} onClick={() => setOpenModal(true)}/>
+            <ModalMovie 
+                isOpen={openModal}
+                setModalOpen = {() => setOpenModal(!openModal)}
+                id = {id} 
+                genre_ids = {genre_ids} 
+                poster = {poster}
+                title = {title}
+                release_date = {release_date}
+                overview = {overview}
+                vote_average = {vote_average}
+                />
         </li>
     )
 }
