857261615e43d94026fa41c58c9ae5e223b91395
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2023-10-12 15:04:39 -0300

    Feito a lógica para mudar o Carrossel
    Agora quando o tamanho da tela for menor que 1000px, ele irá mudar para o carrossel que se meche ao arrastar o para o lado  e quando for maior que 1000px, irá utilizar os botões para mover o conteúdo do carrossel para a direita ou para a esquerda

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -1,57 +1,74 @@
-import { motion } from 'framer-motion'
-import styles from './Carrosell.module.css'
-import { useEffect, useRef, useState } from 'react'
-import BotaoCarrossel from '../BotaoCarrossel'
-import { FaGreaterThan, FaLessThan } from 'react-icons/fa'
-import { ContainerDivParaTituloDosVideos } from '../Containers/ContainerDiv'
+import { motion } from 'framer-motion';
+import styles from './Carrossel.module.css';
+import { useEffect, useRef, useState } from 'react';
+import BotaoCarrossel from '../BotaoCarrossel';
+import { FaGreaterThan, FaLessThan } from 'react-icons/fa';
+import { ContainerDivParaTituloDosVideos } from '../Containers/ContainerDiv';
 
-function Carrossel( {children}: any ) {
-    const carrosel = useRef<any>()
-    const [width, setWidth] = useState<number>(0)
-     useEffect(() => {
-        
-        setTimeout(() => setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth), 500 ) 
-    }, [])
+function Carrossel({ children }: any) {
+  const carrosel = useRef<any>();
+  const [width, setWidth] = useState<number>(0);
+  const [larguraDaTela, setLarguraDaTela] = useState(window.innerWidth);
 
-    function ParaADireita(e: Event) {
-        e.preventDefault()
-        carrosel.current.scrollLeft += carrosel.current.offsetWidth
-        useEffect(() => {
-        
-            setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth) 
-        }, [])
-    
-    }
+  useEffect(() => {
+    const handleResize = () => {
+      setLarguraDaTela(window.innerWidth)
+      setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth)
+    };
 
-    function ParaAEsquerda(e: Event) {
-        e.preventDefault()
-        carrosel.current.scrollLeft -= carrosel.current.offsetWidth
-        useEffect(() => {
-        
-            setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth) 
-        }, [])
-    
-    }
-    
+    window.addEventListener('resize', handleResize);
 
-    return (
-        <>
-            <ContainerDivParaTituloDosVideos>
-                <h1 style={{marginTop:"10px"}}>Trending</h1>
-                <div>
-                    <BotaoCarrossel onClick={(e: Event) => ParaAEsquerda(e)}><FaLessThan /></BotaoCarrossel>
-                    <BotaoCarrossel onClick={(e: Event) => ParaADireita(e)}><FaGreaterThan /></BotaoCarrossel>
-                </div>
-            </ContainerDivParaTituloDosVideos>
-            
-            <motion.div ref={carrosel} className={styles.carrossel} whileTap={{cursor: "grabbing"}}>
-                <motion.ul className={`${styles.inner}`}  drag="x" dragConstraints={{right: 0, left: -width}} initial={{x: 100}} animate={{x: 0}} transition={{duration: 0.3}}>
-                {children}
-                </motion.ul>
-            </motion.div>
-        </>
-        
-    )
+    setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth)
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  function handleScroll(offset: number) {
+    carrosel.current.scrollLeft += offset;
+    setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth);
+  }
+
+  return (
+    <>
+      <ContainerDivParaTituloDosVideos>
+        <h1 style={{ marginTop: "10px" }}>Trending</h1>
+        <div>
+          <BotaoCarrossel onClick={() => handleScroll(-carrosel.current.offsetWidth)}>
+            <FaLessThan />
+          </BotaoCarrossel>
+          <BotaoCarrossel onClick={() => handleScroll(carrosel.current.offsetWidth)}>
+            <FaGreaterThan />
+          </BotaoCarrossel>
+        </div>
+      </ContainerDivParaTituloDosVideos>
+      {larguraDaTela >= 984 ? (
+        <div ref={carrosel} className={styles.carrosselDesktop}>
+          <ul className={`${styles.inner}`}>
+            {children}
+          </ul>
+        </div>
+      ) : (
+        <motion.div
+          ref={carrosel}
+          className={styles.carrosselMobile}
+          whileTap={{ cursor: "grabbing" }}
+        >
+          <motion.ul
+            className={`${styles.inner}`}
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            initial={{ x: 100 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            {children}
+          </motion.ul>
+        </motion.div>
+      )}
+    </>
+  );
 }
 
-export default Carrossel
+export default Carrossel;
