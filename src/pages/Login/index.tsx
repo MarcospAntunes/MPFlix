0c020fb9a41e33a73ad18394a7187fcbac241a8e
Marcospantunes <marcospantunes203@gmail.com>
2023-10-27 14:25:37 -0300

    Criação dos formulários
    Montado a estrutura dos formulários de registro e login de usuário. Em breve serão adicionadas as partes funcionais do formulário.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,25 @@
+import { ContainerCentralizado } from "../../components/Containers/ContainerDiv"
+import { InputStyled } from "../../components/Form/Input"
+import Form from "../../components/Form"
+import { Link } from "react-router-dom"
 
 
 function Login() {
   return (
     <>
-    
+      <ContainerCentralizado>
+        <Form action="" method="post">
+          <h1>Login</h1> <br />
+
+          <InputStyled type="email" placeholder="Seu Email" required  />
+          <InputStyled type="password" placeholder="Senha" required minLength={8} />
+          <br />
+          <InputStyled type="submit" value="Login" />
+
+          <p>Don't have account? <Link to={'/register'}>Click here</Link> and create an account!</p>
+        </Form>
+
+      </ContainerCentralizado>
     </>
   )
 }
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom"
+import { ContainerCentralizado } from "../../components/Containers/ContainerDiv"
+import Form from "../../components/Form"  
+import { InputStyled } from "../../components/Form/Input"
 
 
 function Register() {
   return (
     <>
-    
+      <ContainerCentralizado>
+        <Form action="" method="post">
+          <h1>Register</h1> <br />
+
+          <InputStyled type="text" placeholder="Nome Completo" required minLength={4} />
+          <InputStyled type="email" placeholder="Seu Email" required />
+          <InputStyled type="email" placeholder="Confirme seu Email" required />
+          <InputStyled type="password" placeholder="Senha" required minLength={8} />
+          <InputStyled type="password" placeholder="Confirme sua Senha" required minLength={8} />
+          <br />
+          <InputStyled type="submit" value="Registrar" />
+          <p>Already have an account? <Link to={'/login'}>Click here</Link> and login!</p>
+        </Form>
+
+      </ContainerCentralizado>
     </>
   )
 }
