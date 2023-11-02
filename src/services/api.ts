827e20f96a0063c37b6fe78e96c1a18747d87dd1
Marcospantunes <marcospantunes203@gmail.com>
2023-11-02 20:12:51 -0300

    melhoria
    Agora será utilizado variáveis do tipo constante nas requisições para a API ao invés do tipo let.

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,12 +5,12 @@ const api_key = "4fdd778d99f4d99cdcd4b7b84dafb119"
 // Seção Trending
 export async function getTrendingMovieData(type: string, setMovieTrendingData: any): Promise<void> {
     try {
-        let response = await axios.get(`https://api.themoviedb.org/3/trending/${type}/day?api_key=${api_key}&media_type=movie`)
+        const response = await axios.get(`https://api.themoviedb.org/3/trending/${type}/day?api_key=${api_key}&media_type=movie`)
         setMovieTrendingData(response.data.results)
     } catch(error: any) {
         const converteErro = error.toString()
-        let erro = window.document.createElement("div")
-        let conteudo = window.document.createTextNode(converteErro)
+        const erro = window.document.createElement("div")
+        const conteudo = window.document.createTextNode(converteErro)
         erro.appendChild(conteudo)
     }
     
@@ -19,12 +19,12 @@ export async function getTrendingMovieData(type: string, setMovieTrendingData: a
 // Seção Discover
 export async function getDiscoverMovieData(type: string, setMovieDiscoverData: any): Promise<void> {
     try {
-        let response = await axios.get(`https://api.themoviedb.org/3/discover/${type}?api_key=${api_key}`)
+        const response = await axios.get(`https://api.themoviedb.org/3/discover/${type}?api_key=${api_key}`)
         setMovieDiscoverData(response.data.results)
     } catch(error: any) {
         const converteErro = error.toString()
-        let erro = window.document.createElement("div")
-        let conteudo = window.document.createTextNode(converteErro)
+        const erro = window.document.createElement("div")
+        const conteudo = window.document.createTextNode(converteErro)
         erro.appendChild(conteudo)
     }
     
@@ -33,12 +33,12 @@ export async function getDiscoverMovieData(type: string, setMovieDiscoverData: a
 // Filmes em cartaz
 export async function getNowPlayingMovieData(type: string, setNowPlayingMovieData: any): Promise<void> {
     try {
-        let response = await axios.get(`https://api.themoviedb.org/3/${type}/now_playing?api_key=${api_key}`)
+        const response = await axios.get(`https://api.themoviedb.org/3/${type}/now_playing?api_key=${api_key}`)
         setNowPlayingMovieData(response.data.results)
     } catch(error: any) {
         const converteErro = error.toString()
-        let erro = window.document.createElement("div")
-        let conteudo = window.document.createTextNode(converteErro)
+        const erro = window.document.createElement("div")
+        const conteudo = window.document.createTextNode(converteErro)
         erro.appendChild(conteudo)
     }
 }
@@ -46,12 +46,12 @@ export async function getNowPlayingMovieData(type: string, setNowPlayingMovieDat
 // Gêneros dos filmes
 export async function getMovieGenreData(type: string, setMovieGenreData: any): Promise<void> {
     try {
-        let response = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${api_key}`)
+        const response = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=${api_key}`)
         setMovieGenreData(response.data.genres)
     } catch(error: any) {
         const converteErro = error.toString()
-        let erro = window.document.createElement("div")
-        let conteudo = window.document.createTextNode(converteErro)
+        const erro = window.document.createElement("div")
+        const conteudo = window.document.createTextNode(converteErro)
         erro.appendChild(conteudo)
     }
 }
@@ -79,12 +79,12 @@ export async function getAllMovies(type: string, setAllMovies: any): Promise<voi
 // Videos dos filmes
 export async function getVideosMovieData(type: string, setVideosMovieData: any, movie_id: number): Promise<void> {
     try {
-        let response = await axios.get(`https://api.themoviedb.org/3/${type}/${movie_id}/videos?api_key=${api_key}`)
+        const response = await axios.get(`https://api.themoviedb.org/3/${type}/${movie_id}/videos?api_key=${api_key}`)
         setVideosMovieData(response.data.results)
     } catch(error: any) {
         const converteErro = error.toString()
-        let erro = window.document.createElement("div")
-        let conteudo = window.document.createTextNode(converteErro)
+        const erro = window.document.createElement("div")
+        const conteudo = window.document.createTextNode(converteErro)
         erro.appendChild(conteudo)
     }
     
