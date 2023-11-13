715bf1f740482923311e9bdda058f56839bf4b34
Marcospantunes <marcospantunes203@gmail.com>
2023-11-13 19:50:11 -0300

    Update index.tsx

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -6,58 +6,69 @@ import { FaGreaterThan, FaLessThan } from 'react-icons/fa';
 import { ContainerDivParaTituloDosVideos } from '../Containers/ContainerDiv';
 
 interface CarrosselProps {
-  children: any
-  secao: string
+  children: any;
+  secao: string;
 }
-
 
 function Carrossel({ children, secao }: CarrosselProps) {
   const carrosel = useRef<any>();
   const [width, setWidth] = useState<number>(0);
   const [larguraDaTela, setLarguraDaTela] = useState(window.innerWidth);
+  const [listaWidth, setListaWidth] = useState<number>(0);
 
   useEffect(() => {
     const handleResize = () => {
-      setLarguraDaTela(window.innerWidth)
-      setTimeout(() => setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth), 600)
-      
+      setLarguraDaTela(window.innerWidth);
+      setListaWidth(carrosel.current?.scrollWidth || 0);
     };
 
-    window.addEventListener('resize', handleResize);
+    const handleMutation = () => {
+      setListaWidth(carrosel.current?.scrollWidth || 0);
+    };
 
-    setTimeout(() => setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth), 600)
-    setTimeout(() => setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth), 1000)
-    
+    const resizeObserver = new ResizeObserver(handleMutation);
+
+    window.addEventListener('resize', handleResize);
+    resizeObserver.observe(carrosel.current);
+
+    setListaWidth(carrosel.current?.scrollWidth || 0);
 
     return () => {
       window.removeEventListener('resize', handleResize);
+      resizeObserver.disconnect();
     };
   }, []);
 
   function handleScroll(offset: number) {
     carrosel.current.scrollLeft += offset;
-    setTimeout(() => setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth), 600)
-    
   }
+
+  useEffect(() => {
+    setWidth(listaWidth - carrosel.current?.offsetWidth);
+  }, [larguraDaTela, listaWidth]);
 
   return (
     <>
       <ContainerDivParaTituloDosVideos>
         <h1 style={{ marginTop: "10px" }}>{secao}</h1>
         <div>
-          <BotaoCarrossel larguraDaTela = {larguraDaTela} onClick={() => handleScroll(-carrosel.current.offsetWidth)}>
+          <BotaoCarrossel
+            larguraDaTela={larguraDaTela}
+            onClick={() => handleScroll(-carrosel.current.offsetWidth)}
+          >
             <FaLessThan />
           </BotaoCarrossel>
-          <BotaoCarrossel larguraDaTela = {larguraDaTela} onClick={() => handleScroll(carrosel.current.offsetWidth)}>
+          <BotaoCarrossel
+            larguraDaTela={larguraDaTela}
+            onClick={() => handleScroll(carrosel.current.offsetWidth)}
+          >
             <FaGreaterThan />
           </BotaoCarrossel>
         </div>
       </ContainerDivParaTituloDosVideos>
       {larguraDaTela >= 984 ? (
         <div ref={carrosel} className={styles.carrosselDesktop}>
-          <ul className={`${styles.inner}`}>
-            {children}
-          </ul>
+          <ul className={`${styles.inner}`}>{children}</ul>
         </div>
       ) : (
         <motion.div
@@ -77,7 +88,6 @@ function Carrossel({ children, secao }: CarrosselProps) {
           </motion.ul>
         </motion.div>
       )}
-      {}
     </>
   );
 }
