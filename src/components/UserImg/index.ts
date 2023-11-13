85413433c745a526d5edc8712f20ca7fec7b3019
Marcospantunes <marcospantunes203@gmail.com>
2023-11-13 19:30:46 -0300

    Foto de usuário
    Antes este era um componente apenas da página home, porém agora é um componente que será utilizado na página de favoritos, então transferi para a pasta de components.

---
+++ src/components/UserImg/index.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ImgStyled = styled.img`
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  width: 50px;
+  height: 50px;
+  border: 2px solid #bf4343;
+  background-color: #9b9b9b;
+  border-radius: 50%;
+
+`
