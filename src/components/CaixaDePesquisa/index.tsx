c62aee402aec8b242a865aac3c2c30f7967f5267
Marcospantunes <marcospantunes203@gmail.com>
2023-11-16 10:49:16 -0300

    Nova feature
    O componente de pesquisa agora recebe um estado de busca e altera ele, possibilitando pesquisar filmes de acordo com o que é digitado na caixa de pesquisa

--- src/components/CaixaDePesquisa/index.tsx
+++ src/components/CaixaDePesquisa/index.tsx
@@ -1,16 +1,28 @@
 import { BiSearch } from 'react-icons/bi'
 import { DivBusca } from './styles'
 
+interface CaixaDePesquisaProps {
+    search: string
+    setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
 function chamarInput(): void {
     const chamarInput: HTMLInputElement = document.querySelector('#textBusca')!
     chamarInput.focus()
 }
 
-function CaixaDePesquisa(): JSX.Element {
+function CaixaDePesquisa({ search, setSearch }: CaixaDePesquisaProps): JSX.Element {
+    
     return(
         <DivBusca>
             <BiSearch className="btnBusca" onClick={() => chamarInput()}/>
-          <input type="text" id="textBusca" placeholder="Search here" />
+            <input 
+                type="text" 
+                id="textBusca" 
+                placeholder="Search here"
+                value={search} 
+                onChange={(e) => setSearch(e.target.value)} 
+            />
 
         </DivBusca>
     )
