11b84758486bd648eec58d191b948c1e52bad531
Marcospantunes <marcospantunes203@gmail.com>
2023-11-21 15:52:34 -0300

    Removido Função
    Removida as funções verificaSeCampoEmailEhIgual e verificaSeCampoSenhaEhIgual pois elas não são mais necessárias.

--- src/utils/validaFormFunctions.ts
+++ src/utils/validaFormFunctions.ts
@@ -1,10 +1,5 @@
 import { NavigateFunction } from "react-router-dom"
 
-const confirmaEmail: HTMLInputElement = document.querySelector('#confirmaEmail')!
-const confirmaSenha: HTMLInputElement = document.querySelector('#confirmaSenha')!
-const senha: HTMLInputElement = document.querySelector('#senha')!
-const email: HTMLInputElement = document.querySelector('#email')!
-
 interface handleLoginProps {
     e: React.MouseEvent<HTMLInputElement, MouseEvent> 
     setError: React.Dispatch<React.SetStateAction<string>>
@@ -27,27 +22,6 @@ interface handleRegisterProps {
 }
 
 export function VerificaSeCampoEmailEhIgual() {
-    const errorEmail: HTMLInputElement = document.querySelector('#errorEmail')!
-    
-    if(confirmaEmail.value !== email.value ) {
-        errorEmail.style.display = 'block'
-        
-    } else if (confirmaEmail.value === '' || confirmaEmail.value === email.value ) {
-        errorEmail.style.display = 'none'
-    }
-}
-
-export function VerificaSeCampoSenhaEhIgual() {
-    const errorSenha: HTMLElement = document.querySelector('#errorSenha')!
-
-    if(confirmaSenha.value !== senha.value ) {
-        errorSenha.style.display = 'block'
-
-    } else if (confirmaSenha.value === '' || confirmaSenha.value === senha.value ) {
-        errorSenha.style.display = 'none'
-    }   
-}
-
 export const handleLogin = ({ e, setError, email, password, login, navigate }: handleLoginProps) => {
     e.preventDefault()
     if(!email || !password) {
